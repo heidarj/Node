314a0c5f3c76cfba1ebb1e59b953e694fd81209f
heidarj <heidarj@hraunteigur5.com>
2015-11-26 13:23:43 +0000

    Test
    Added IP variable, which tries to detect whether there is an active ssh
    session on the host running node and if so set the IP to something else
    than 127.0.0.1
    
    added node_modules to .gitignore

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+var IP = "127.0.0.1"
+
+if (process.env.SSH_CONNECTION != null) {
+        
+        IP = "162.243.39.179"
+        
+}
+
+console.log(IP)
 
 var http = require('http')
+var express = require('express')
 
 http.createServer(function(req, res) {
 
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('hello world!');
 
-}).listen(80, '162.243.75.119');
+}).listen(80, '127.0.0.1');
+
